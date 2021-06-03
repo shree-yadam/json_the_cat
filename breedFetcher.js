ce1379627dd3a1cbe318aaef14c9552d3e5ed1dd
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-03 19:24:39 +0000

    modularized code and added mocha test cases using chai assertion for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,18 @@
 const request = require("request");
 
-const getCatInfo = function(breedName) {
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-      switch (error.code) {
-      case 'ENOTFOUND':
-        console.log("Address not found!");
-        break;
-      default:
-        console.log(`Error (${error.code})`);
-      }
+      callback(error, null);
     } else {
       const data = JSON.parse(body);
-      console.log(data[0].description);
+      if (data.length === 0) {
+        callback(new Error("Breed Not Found"), null);
+      } else {
+        callback(null, data[0].description);
+      }
     }
   });
 };
 
-const getUserInput = function() {
-  if (process.argv.length <= 2) {
-    console.log("Please enter a breed name to search for.");
-    process.exit();
-  }
-  const breedName = process.argv[2];
-  getCatInfo(breedName);
-};
-
-getUserInput();
+module.exports = {fetchBreedDescription};
---
+++ index.js
@@ -0,0 +1,18 @@
+const { fetchBreedDescription } = require("./breedFetcher");
+
+const getUserInput = function() {
+  if (process.argv.length <= 2) {
+    console.log("Please enter a breed name to search for.");
+    process.exit();
+  }
+  const breedName = process.argv[2];
+  fetchBreedDescription(breedName, (error, desc) => {
+    if (error) {
+      console.log(`Error (${error.message})`);
+    } else {
+      console.log(desc);
+    }
+  });
+};
+
+getUserInput();
